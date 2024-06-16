8bdce79db27d0d908f2ec13d333f9e987b6218c0
HOSHINOYUYA <125641044+HOSHINOYUYA@users.noreply.github.com>
2024-06-16 19:32:35 +0800

    Add files via upload

---
+++ ch3_homework.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+<meta charset="utf-8">
+<title>ch3作業</title>
+</head> 
+<br>
+<h3>HTML網頁</h3>
+<hr/>
+<img src="sample.jpg" width=400 height=300 />
+<ol>
+    <li>a</li>
+    <li>b</li>
+    <li>c</li>
+</ol>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+<dl>
+    <dt>flower</dt>
+    <dd>花</dd>
+    <dt>water</dt>
+    <dd>水</dd>
+</dl>
+<a href="https://github.com/HOSHINOYUYA" target="frame_name">YUYA github</a>
+</br>
+
+<caption>translation</caption>
+<table border="1">
+<tr><td colspan="2">翻譯</td></tr>
+<tr>
+   <td headers="client">water</td>
+   <td headers="server">flower</td>
+</tr>
+<tr>
+   <td>水</td>
+   <td>花</td>
+</tr>
+</table>
+<form action="ch3_homework2.php" method="post">
+姓名:<input type="text" name="sName" placeholder="Please input"><br/>
+密碼:<input type="password" name="sPWD" required><br/>
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
---
+++ ch3_homework2.php
@@ -0,0 +1,8 @@
+<meta charset=utf8>
+
+<?php
+    $sName=$_POST["sName"];
+    echo "您的姓名:".$sName."<br/>";
+    $sPWD=$_POST["sPWD"];
+    echo "您的密碼".$sPWD."<br/>";
+?>
